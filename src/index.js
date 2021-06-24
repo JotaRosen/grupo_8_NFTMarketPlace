a7ec60b45ab206d80b92958fb9d259e79f49bfc2
JotaRosen <jrosenfeld@fi.uba.ar>
2021-06-24 20:54:25 -0300

    Git ignore & folder structures | sprint 2

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+let port = process.env.PORT || 3000;
+
+let app = express();
+
+
+//Public access
+app.use('/public',express.static(path.resolve(__dirname, '../public')));
+
+// Websites Routes
+//Will have many routes 
+app.use(require('./routes/web'));
+
+//Listening to port ${port}
+app.listen(port, ()=> console.log('Server up on http://localhost:' + port));
---
+++ src/routes/web.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express.Router();
+const path = require('path');
+//Routing list
+app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"../views","home.html"))); 
+app.get("/login",(req,res) => res.sendFile(path.resolve(__dirname,"../views","login.html")));
+app.get("/register",(req,res) => res.sendFile(path.resolve(__dirname,"../views","register.html")));
+app.get("/market",(req,res) => res.sendFile(path.resolve(__dirname,"../views","market.html")));
+app.get("/megusta",(req,res) => res.sendFile(path.resolve(__dirname,"../views","home.html")));  
+
+module.exports = app;
